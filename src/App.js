8be7474ffb9131b98341b7bc44608f0650a02a46
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2020-04-08 20:40:58 +0100

    install gh-pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Some stuff about musical moddes to follow here. Maybe.
         </p>
         <a
           className="App-link"
