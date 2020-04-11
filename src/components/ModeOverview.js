721fb662f45f2ac9778120298a72c5e24540385f
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2020-04-11 10:14:43 +0100

    factor out to ModeOverview

--- src/components/ModeOverview.js
+++ src/components/ModeOverview.js
@@ -23,5 +23,27 @@ export default props => (
   <div className={`mode-overview mode-overview-${props.modeName}`}>
     <h3>The {props.modeName} Mode</h3>
     {renderTable(props.chords)}
+    <div className='mode-detail'>
+      <h3 className='mode-detail-subheader'>
+        What is it?
+      </h3>
+      {props.whatIsIt()}
+      <h3 className='mode-detail-subheader'>
+        What does it sound like?
+      </h3>
+      {props.soundsLike()}
+      <h3 className='mode-detail-subheader'>
+        How do I spot/write {props.article} {props.modeName} progression?
+      </h3>
+      {props.spotIt()}
+      <h3 className='mode-detail-subheader'>
+        How do I solo over it?
+      </h3>
+      {props.soloOverIt()}
+      <h3 className='mode-detail-subheader'>
+        Where can I hear it?
+      </h3>
+      {props.hearIt()}
+    </div>
   </div>
 )
