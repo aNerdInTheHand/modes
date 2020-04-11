aa99c207b05f04925e9c76c82277ea4a8e18bfcf
Nick Holvast <nick.holvast@engineering.digital.dwp.gov.uk>
2020-04-11 10:18:52 +0100

    add placeholder Ionian text to all modes

--- src/pages/Dorian.js
+++ src/pages/Dorian.js
@@ -9,8 +9,50 @@ const content = () => (
   <div className='mode-section'>
     <ModeOverview
       modeName={'Dorian'}
-      chords={dorianChords} />
+      chords={dorianChords}
+      article={'a'}
+      whatIsIt={() => whatIsIt()}
+      soundsLike={() => soundsLike()}
+      spotIt={() => spotIt()}
+      soloOverIt={() => soloOverIt()}
+      hearIt={() => hearIt()} />
   </div>
+)
+
+const whatIsIt = () => (
+  <p>
+    The Ionian mode is the first degree mode of the major scale. In other words, its scale is the major scale starting from the first note of the major scale.
+    In (other) other words, its scale is just the major scale.
+    Its intervals are T-T-S-T-T-T-S.
+  </p>
+)
+
+const soundsLike = () => (
+  <p>
+    The Ionian mode is happy, as you'd expect from a mode with all perfect and major intervals.
+    Don't expect too much drama from this mode - lots of nice, soft major sevenths on both the I and the IV are the order of the day.
+  </p>
+)
+
+const spotIt = () => (
+  <p>
+    Keep it simple, stupid. I-IV-V-I will work nicely with a major 7 chord on the I.
+    To really pin down the Ionian flavour, use a second inversion on the IV chord (this uses the I note of the scale in the root of your IV chord) and a sus4 on your V chord (which again uses that I note).
+    If you want to get really wild, make the V dominant, but don't come crying to me if you just end up smashing wild Mixolydian phrases over it.
+  </p>
+)
+
+const soloOverIt = () => (
+  <p>
+    Softly softly catchy monkey. If you're playing over an Ionian progression, you're probably not going to be smashing out whammy bar dives with the gain up to 11.
+    But hey, don't let me stop you. To really get the feel of the Ionian mode though, you want to really play on the half-step between the major 7 and the I (or octave).
+  </p>
+)
+
+const hearIt = () => (
+  <p>
+    You'll hear Ionian music in lots of places - think nursery rhymes, Christmas carols, sentimental music, live lounge acoustic music.
+  </p>
 )
 
 const title = () => <div className='subtitle'>Dorian</div>
